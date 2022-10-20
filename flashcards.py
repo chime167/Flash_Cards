7a20dd4ba99e78c8bd589f23201a14a05a7766e0
chime167 <chimerame@protonmail.com>
2022-10-20 19:24:16 -0400

    added docstring

--- flashcards.py
+++ flashcards.py
@@ -5,6 +5,20 @@ from io import StringIO
 import argparse
 
 
+'''CLI parameters:
+        -import_from: file you want to import cards from to this session
+        -export_to: file you want to write to when done
+    Input parameters:
+        -add: you will be prompted to write first a term and then its definition
+        -remove: you will be prompted to write the term of the card you want to delete
+        -import: enter the file of flash cards you want imported to the session
+        -export: enter the file you want to write this session's flash cards to
+        -ask: enter the number of times you want to guess a card
+        -log: enter the name of the file you want to write a log of the entire session to
+        -hardest card: tells you which card(s) you've missed the most
+        -reset stats: resets the count for how many times you've missed cards'''
+
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--import_from', help='Enter file to import flashcards from')
 parser.add_argument('--export_to', help='Enter file to export new flashcards to')
